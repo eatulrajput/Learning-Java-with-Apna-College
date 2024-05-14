f9684e46a49f15bbe2928101c81dd5133d0da704
Atul Rajput <eatulrajput@gmail.com>
2024-05-14 13:19:38 +0530

    Updating Repository

---
+++ variables.java
@@ -0,0 +1,21 @@
+public class variables {
+    public static void main(String[] args) {
+      //variables
+      String name = "Tony Stark";
+      int age = 48;
+      double price = 34.44;
+      int a = 23;
+      int b = 43;
+      b = 20;
+      name = "Ironman";
+ 
+      // print variables
+      System.out.println("Name: " + name);
+      System.out.println("Age: " + age);
+      System.out.println("Price: " + price);
+      System.out.println("a: " + a);
+      System.out.println("b: " + b);
+
+    }
+ }
+ 

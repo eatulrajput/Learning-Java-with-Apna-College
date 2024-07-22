909e848103a1d5224e791da0d3331633c19584d8
Atul Rajput <eatulrajput@gmail.com>
2024-07-22 08:52:59 +0530

    Updating Repository

--- recursion/question2.java
+++ recursion/question2.java
@@ -1,4 +1,14 @@
 // Print sum of n natural numbers
-public class question2 {
-    
-}
+public class question1 {
+        public static void printNumb(int n){
+            if (n == 0) {
+                return;
+            }
+            System.out.println(n);
+            printNumb(n-1);
+        }
+        public static void main(String[] args) {
+            int n = 5;
+            printNumb(n); // where n is 5
+        }
+    }

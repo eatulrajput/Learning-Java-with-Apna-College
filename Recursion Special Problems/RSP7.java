21781bfd260735b855601dcb148b822db34e80a4
Atul Rajput <eatulrajput@gmail.com>
2024-08-04 16:09:30 +0530

    Updating Repository

--- Recursion Special Problems/RSP7.java
+++ Recursion Special Problems/RSP7.java
@@ -1,3 +1,21 @@
+/*
+ * Print all the subsequences of a string.
+ */
 public class RSP7 {
-    
+    public static void subsequences(String str, int idx, String newString){
+        if (idx == str.length()) {
+            System.out.println(newString);
+            return;
+        }
+        char currChar =  str.charAt(idx);
+
+        subsequences(str, idx+1, newString+currChar);
+
+        subsequences(str, idx+1, newString);
+    }
+
+    public static void main(String[] args) {
+        String str ="abc";
+        subsequences(str, 0, "");
+    }
 }
---
+++ Recursion Special Problems/RSP8.java
@@ -0,0 +1,31 @@
+/*
+ * Print all the unique subsequences of a string.
+ */
+
+import java.util.HashSet;
+
+public class RSP8 {
+    public static void subsequences(String str, int idx, String newString, HashSet<String> set){
+        if (idx == str.length()) {
+            if (set.contains(newString)) {
+                return;
+            }
+            else{
+                System.out.println(newString);
+                set.add(newString);
+                return;
+            }
+        }
+        char currChar =  str.charAt(idx);
+
+        subsequences(str, idx+1, newString+currChar, set);
+
+        subsequences(str, idx+1, newString, set);
+    }
+
+    public static void main(String[] args) {
+        String str = "aaa";
+        HashSet<String> set = new HashSet<>();
+        subsequences(str, 0, "", set);
+    }
+}

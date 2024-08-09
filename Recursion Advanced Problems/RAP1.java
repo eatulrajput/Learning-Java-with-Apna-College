ebbcd1536aa91897748f7cb5198fc598bebe5af2
Atul Rajput <eatulrajput@gmail.com>
2024-08-09 22:26:31 +0530

    Updating Repository

--- Recursion Advanced Problems/RAP1.java
+++ Recursion Advanced Problems/RAP1.java
@@ -1,3 +1,26 @@
+/*
+ * Print all permutations of a string
+ */
+
 public class RAP1 {
-    
+
+    public static void printPerm(String str, String permutation){
+
+        if (str.length() == 0) {
+            System.out.println(permutation);
+        }
+
+        for (int i = 0; i < str.length(); i++) {
+            char currChar = str.charAt(i);
+
+            String newStr = str.substring(0, i) + str.substring(i+1);
+            printPerm(newStr, permutation + currChar);
+
+        }
+    }
+
+    public static void main(String[] args){
+        String str = "abc";
+        printPerm(str, "");
+    }
 }
---
+++ Recursion Advanced Problems/RAP2.java
@@ -0,0 +1,6 @@
+/*
+ * Count total paths in a maze to move from (0,0) to (n,m)
+ */
+public class RAP2 {
+    
+}
